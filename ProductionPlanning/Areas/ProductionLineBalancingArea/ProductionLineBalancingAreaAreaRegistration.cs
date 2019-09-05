a4e945d2a258f4263472d3d2e8ab36cc0757a797
cameliamaleki <54914823+cameliamaleki@users.noreply.github.com>
2019-09-05 15:06:22 +0430

    Add files via upload

---
+++ ProductionPlanning/Areas/ProductionLineBalancingArea/ProductionLineBalancingAreaAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CRM.Areas.ProductionLineBalancingArea
+{
+    public class ProductionLineBalancingAreaAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "ProductionLineBalancingArea";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "ProductionLineBalancingArea_default",
+                "ProductionLineBalancingArea/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
